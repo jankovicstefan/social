0aa4aab57665415186b3fc49f27cdcccab23fde9
Stefan <mng.stefan@gmail.com>
2018-02-06 02:43:21 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,20 +8,13 @@ import { MainComponent } from './main/main.component';
 import { RouterModule, Routes } from '@angular/router';
 
 
-
-const routes: Routes = [
-  // { path: '', redirectTo: '/main', pathMatch: 'full'},
-  // { path: 'main', component: MainComponent},
-  //{ path: 'detail/:id', component: MainComponent }
-];
-
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
   ],
   imports: [
-    RouterModule.forRoot(routes),
+    
     BrowserModule,
     FormsModule,
     HttpModule
